bc7d4ccb4dfcc19222792c82d4204ac8453aafe7
Xnyll <47606240+Xnyll@users.noreply.github.com>
2024-03-06 18:38:44 +0000

    Update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,25 +14,19 @@ const Home = () => {
           <motion.header {...slideAnimation('down')}>
             <img src='./threejs.png' alt='logo' className='w-8 h-8 object-contain'/>
           </motion.header>
-
         <motion.div className='home-content' {...headContainerAnimation}>
-
           <motion.div {...headTextAnimation}>
             <h1 className='head-text'>
               LET'S <br className='xl:block hidden'/> DO IT.
             </h1>
           </motion.div>
-
           <motion.div {...headContentAnimation} className='flex flex-col gap-5'>
             <p className='max-w-md font-normal text-gray-600 text-base'>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium fugit laudantium facere magni cum nulla itaque sed voluptatem quia alias accusamus atque aliquam quae, eum quidem minima neque non distinctio?
             </p>
-
             <CustomButton type="filled" title="Customize it" handleClick={() => state.intro = false} customStyles="w-fit px-4 py-2.5 font-bold text-sm"/>
           </motion.div>
-          
         </motion.div>
-
         </motion.section>
       )}
       </AnimatePresence>
